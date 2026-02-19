c1aec4d7d8cca748ecf7ab726fdd44aeebcff196
Derkaoui05 <147924188+Derkaoui05@users.noreply.github.com>
2026-02-19 12:24:19 +0000

    add blan taret for the portfolio link'

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -184,7 +184,7 @@ const FooterBlock: React.FC = () => {
       </div>
       <div className="py-3 bg-gray-100 dark:bg-gray-900">
         <div className="max-w-3xl mx-auto px-5 sm:px-10 md:px-12 lg:px-5 flex justify-center text-center">
-          <p> © 2026 Karmel. Developed By <span className="text-red-500"><Link href="https://derkaoui.netlify.app/">Derkaoui Yassir</Link></span> </p>
+          <p> © 2026 Karmel. Developed By <span className="text-red-500"><Link target="_blank" href="https://derkaoui.netlify.app/">Derkaoui Yassir</Link></span> </p>
         </div>
       </div>
     </footer>
